c1a2a70a95181653065600943c81c0b2313dc3de
Asep5K <akunreza292@gmail.com>
2025-09-03 21:55:23 +0700

    remove blablablabla

--- eww/scripts/menuctl.sh
+++ eww/scripts/menuctl.sh
@@ -4,5 +4,5 @@ if [[ -z $(eww active-windows | grep 'menuctl') ]]; then
     /usr/bin/eww open menuctl && /usr/bin/eww update menurev=true
 else
     /usr/bin/eww update menurev=false
-    (sleep 0.2 && /usr/bin/eww close menuctl) &
+    (sleep 0.1 && /usr/bin/eww close menuctl) &
 fi
