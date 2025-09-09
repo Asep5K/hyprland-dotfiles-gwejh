6b32331977a79df3baf2f37c3d647fc0d1517ef9
Asep5K <akunreza292@gmail.com>
2025-09-09 10:24:31 +0700

    add waybar

---
+++ waybar/scripts/dnd.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+f="/var/lib/AccountsService/icons/$USER"
+
+# Toggle DND
+if makoctl mode | grep -q "do-not-disturb"; then
+    notify-send -i "$f" "Mode DND Off"
+    makoctl mode -r do-not-disturb
+else
+    notify-send -i "$f" "Mode DND On"
+    sleep 5
+    makoctl mode -a do-not-disturb
+fi
+
